d6c00edaa4645a61fcbd4cda9a5142c21b9a3222
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-09 20:12:11 -0500

    API_winnowing SpringBoot

---
+++ API/src/main/java/TT2018B003/comp3/API/ApiApplication.java
@@ -0,0 +1,13 @@
+package TT2018B003.comp3.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
