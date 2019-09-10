d80082f78e0af6b8ca6d56ddc8903e1f9b03fcf2
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-09 22:24:10 -0500

    Winnowing service

---
+++ API/src/main/java/TT2018B003/comp3/API/Service/WinnowingService.java
@@ -0,0 +1,26 @@
+package TT2018B003.comp3.API.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WinnowingService implements IWinnowing {
+
+	private String chaffing;
+	private String pattern;
+	
+	@Override
+	public void setChaffing(String chaffing) {
+		this.chaffing = chaffing;
+	}
+	
+	@Override
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+
+	@Override
+	public String makeWinnowing() {
+		return "Hola";
+	}
+
+}
