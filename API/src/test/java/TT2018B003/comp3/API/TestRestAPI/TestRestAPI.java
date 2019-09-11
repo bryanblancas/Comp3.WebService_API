de25c318480e0a298dc5308a461347bff7a1ca2a
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-10 22:01:35 -0500

    Class for test the API

--- API/src/test/java/TT2018B003/comp3/API/TestRestAPI/TestRestAPI.java
+++ API/src/test/java/TT2018B003/comp3/API/TestRestAPI/TestRestAPI.java
@@ -1,14 +1,31 @@
 package TT2018B003.comp3.API.TestRestAPI;
 
 import org.springframework.web.client.RestTemplate;
+import TT2018B003.comp3.API.Domain.WinnowingModelTest;
 
 public class TestRestAPI {
 	
 	public static void main(String[] args) {
 		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8080/API/v1/winnowing";
+
+		WinnowingModelTest data = new WinnowingModelTest();
+		data.setChaffing("Hola mundo chaffing");
+		data.setPattern("pattern");
 		
-		String url = "http://localhost:8080/API/v1/chaffing/holamundo";
-		String a = restTemplate.getForObject(url, String.class);
+		String a = restTemplate.postForObject(url, data, String.class);
 		System.out.println(a);
+		
+		/*user.setEmail("admin@gmail.com");
+		user.setFirstName("admin");
+		user.setLastName("admin");
+		user.setCreatedBy("admin");
+		user.setUpdatedBy("admin");
+		ResponseEntity<User> postResponse = restTemplate.postForEntity(getRootUrl() + "/users", user, User.class);
+		Assert.assertNotNull(postResponse);
+		Assert.assertNotNull(postResponse.getBody());
+		*/
+		
+		
 	}
 }
