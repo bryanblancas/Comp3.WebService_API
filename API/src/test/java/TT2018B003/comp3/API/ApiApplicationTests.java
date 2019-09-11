1dadfbdc53990a37247455468cee985269eeac3e
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-10 22:01:22 -0500

    A properly class for test the API

--- API/src/test/java/TT2018B003/comp3/API/ApiApplicationTests.java
+++ API/src/test/java/TT2018B003/comp3/API/ApiApplicationTests.java
@@ -1,41 +1,59 @@
 package TT2018B003.comp3.API;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
+import TT2018B003.comp3.API.Domain.WinnowingModelTest;
+
+//@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class ApiApplicationTests {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
-	
-	@LocalServerPort
-	
-	private int port;
+	//@Autowired
+	private RestTemplate restTemplate = new RestTemplate();
 	
 	@Test
 	public void contextLoads() {}
-
+	private static String urlBase;
+	
+	@BeforeClass
+	public static void setUrlBase() {
+		urlBase = "http://localhost:8080/API/v1/";
+	}
+	
 	@Test
-	public void testSetChaffing() {
-		HttpHeaders headers = new HttpHeaders();
-		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	public void testGetWinnowingWithData() {
 		
-		ResponseEntity<String> response = restTemplate.exchange("localhost:8081/API/v1/chaffing/HolaMundo",
-				HttpMethod.GET, entity, String.class);
+		String chaffing = "JtJplCHhSEMh0K5Ga10RO9lUkgVHJNWpx0upGkQVmag63eDTMtyMQ7FZQmQ0283VIdhoOSSxrnSixVLKJdvrJMuhbGMnEKoUqrEEUSDd7HVLDXDDPZCpZAlOTpXIpAplKEDa5hXLGlEcnroQ0SzRimUKRZLCkKskcmkw6DiXkxHhsEQ9mBTyBOcnlcgki42awjXMrUaqndhqMR6oqrWawTfR6JOKVQpDVbddCJMtyKR6tUI0PehEiuUI1ijUqUSihSxGKkyJZYJRqMUn06iXuh0CMSrRqBd73QzSK5OoorBgMDwS6ESLtQRnNdytBlslDxzd5oQSDSJFK9Hq9RNBNCvZjmXK4EoYnKJgyjuTrSQrZZrVIZvMJTkM8DGQ5tZTKGQYnekGww0czSqa7nT9iMUYk6hFi92caiZNinbDLZzNNZBu9PoIIt7JlKLFbs41k2aNLmohmGch9KUmk6iWVQ7BGFVsqolQzUFVKuY7YQh6IFOlic5OKxDIZU7NIRrmFuOVDmw1mM8kqkEawRiR6BNK1QrCO93odSJdLLQw1elnMrWi9zvQ7HWKkSzRKFLoRIq5lLUizU+EI2mCzSGZ6HLZBiDc6VQJqlYqlKaDijkaURod6ESK5QhVlKoaEQjBK5HKk0K5IiqViFZ6NQbZI9DqG8WO0yTYxjQxIsUlEKoltbTaQ9uNgTSyHNqMttJIYmM2zo8U6tGSeFSodiPRstc2HOlG6nlsYioQ6DSKTZK3IhSrFNKt3tpCbQezDYcQqpSqCYTI0KjGOx0KyBCoxJotAtE16LY7HQpCIJONRZKdzm8zXo30eyyRFV8DkcVEaSLNjRYaKRrEOM3u9HoJEGYrm+2zK2FSMjWT7BS76WRBVtvqM4phIOuM6DZjEOj2KdupJEoZELcV36c75SEnhwfJhSDpGJHuJYO5xOIo0Kna3PBlIg2tRdKVxqszlM30ai3W2DVSxiWpYRLjItSmJlvY5sbNyWxEINh3A9baK9rpNjLZSjNQrsqRaNEp0qXJxS7NG9uo5tswZkwzjeaTEabiGZyNN4pNUkoZjKcKBRrTK5uoNau5jGIrm610KqmY6RjN7mRD4Q7xLBDI05pRqNM0jMq2W3M6SumWibbpOZnvBSo9rGoZUanjMUmk5QA3AFMACQAWABMAHQAWAHcAIAAAKwAWAHMAFQAcADIAGABTAC4AVwAKAGUAVAAAOABWgAAEAAAABgBzADkAVABIAAB7AFUAEABJACAGgAwAJwA7AGQAagBAGABTACYAcwA6ADwAVgB5QBgAGQAIABYAGgAtAAoAVwA3gBAAAAAKACEAUABRACAGAAkAdABJAEkAIoAWABkAEAUAdAB5QAwAAHEAPQAMwA4AZwAjAAAADgAyAFCAIAIAWgAgwAAAAgAVAGAEAAAQAIAAAwAMAA8AJQBTABAAXgB5ACSAGQAMAQBBgBEAJoALACAAXgAvACYAGgAVgBCALAAATQAKADACAGEAagBhADEAEgAJAHACQBwAgC4APABYAAAAagBQAHIAaAA3AFEALgBWAAsAaQBigAUANQBQAGgAFwA6AAAKAEAAAFAALAAUAGAJACAAAgAAAATACgAZAAoAdwAkABQAABQAAGACAERACwAUAEYABAB6AAkABABZACIAUQBKAAOAAAUAcIAAAAAAAABqADQAYgBLACwAFQANAAAKABkAHABqACeAGgBOgAYAABQAAG9AEQBUAA0AJAArAFkACoAYADIAVQAJAFQAEYAhAA4AQQAYAFUALIAAAgAgAgB3QAgAVgApAFQAEwAFAAIARgAAFAAHABeAAkAVAAANABgAUgBoAYAAAUAIAAA0AFEASQBmgBABAAAABgAAbQAnACMAAAoAbwAxAFQADQAkACsAWQAIAQAAFgAABgAqAEAIAE0AIAAAJwAAAgAdAAIAMQAqAFkACAAAKwBdAAoAQgAWAABuAGAKAEACAEADABMAKAAQAgA1AAQAQAAAWQBHAAADgB0AAARACABWACkAJIAbAD0ANABJADQAGAKAJgAADQAADgAgAFQAIAQAKkARAAACAAAUACgAFAAjAB0ATgBFQAEAEAAgBABCAGUAYAMAKABTAAUANAAaAGACAEABAA4AKAEAK4AQAGIAcgAtABsACQBTAAoATQBGADkAAAAOAGMAAAAAAgA0AFEATsAUAAAADgA4QBkAUoANAFQAEgAAAgBpABsAXgAPABQAAEwAVgBKACAAAgBlADEAPQAEgARACAIAQAcAAAEAABYAMgAegAAGADMAUAIAYQAlAF0AKAAgAAUAAoADADsADQAAAAMAEYAAAAEAEUAJAHMAAwA1AGwAVwAAEwANACABAFDAEgBVADUAWQANAAAKAB0AJABxACIATQAuAHMAIwApAFJAEAAhABQASQBgAAEAIABkACwAWQBoAEAIAQASAGIAHAAYAEkAEAEAWQAIAEUAGQBNAEAAAgBVABYAbAA4AAAAJQAgCAAASgA9ABIACYAWADsAXQAiAGADAFoACAAVAEsAEQBOAE0ANgBZAEkAYAoAAFIAaQA4ABoAACQAQQBgAABGACADACYAQwAbAAUARIAAAQAMAAA3AEIAQA5ADAEAWQAnABcAQAEAEoAJACUAXgBOABcAAwBFAEAFACAAUQAOAFAAAQBEADgAJAAbAAqAEAABABoANAA5AE0ASQBGAAAAAgAAABAEABYAYQBVQA4ARQAmABYAJgBFABIARQB0AEMANQBOACoARgAASQAEAEAAAF4ADQA1AEMAEACAGQAtAB0ASQBQAUANABAEABgAEwBNAAQAEoAYAFYATQAZAAAmAEYASgAZAEoAMQA3AEwAJQAgCwAQAFYAYAGAEAADAAsAIYAGAFUAIQBaAC8AAwAbADEAIMAQABeAHAAAFAAyABgAAEsAOwAZAGoAdQAiACGAEgA2QAcAUwAJAGYAewAxAEYATgAwAFsAAAM=";
+		String pattern = "   @0@@P@@   @ P@ \"@ @@@!%D@   ! @(@@PD  0  2@@ @L0! @D  1h@@@   @@@ @A@`@0 p   0@@ $ !0 B  `  @@  D@@0@  @@èHH(0@$ @#(@P@HÈ@   %@ \" ÐD8bH@   A!@ @À X  @b @P7  A @@@P `  0!HÑ@$  D@0 @@AÀ % @ @px\" @!X@ $`@` XL  À    @À0\"\" @J@pè  D@ @À ,   B!@  ";
+		String certificate = chaffing+":"+pattern;
 		
-		Assert.assertEquals("ok", response);
+		String url = urlBase+"winnowing";
+		WinnowingModelTest data = new WinnowingModelTest(chaffing, pattern);
+		
+		String a = restTemplate.postForObject(url, data, String.class);
+		
+		Assert.assertNotNull(a);
+		Assert.assertEquals(certificate, a);
+	}
+	
+	@Test
+	public void testGetWinnowingWithoutData() {
+		String url = urlBase+"winnowing";
+		WinnowingModelTest data = new WinnowingModelTest();
+		
+		String a = restTemplate.postForObject(url, data, String.class);
+		
+		Assert.assertNotNull(a);
+		Assert.assertEquals("null:null", a);
 	}
 	
 }
