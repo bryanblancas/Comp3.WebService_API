18056dd3453af8e35afc34c07d4a6ce27b68913e
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-11 13:39:47 -0500

    Implements IUserRepository which is a repository to access to a database

--- Test_WebService/src/main/java/TT_2018B003/Test_WebService/Service/LoginService.java
+++ Test_WebService/src/main/java/TT_2018B003/Test_WebService/Service/LoginService.java
@@ -1,13 +1,25 @@
 package TT_2018B003.Test_WebService.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import TT_2018B003.Test_WebService.Model.Entity.UserEntity;
+import TT_2018B003.Test_WebService.Repository.IUserRepository;
 
 @Service
 public class LoginService {
+	
+	@Qualifier("UserRepositoryManual")
+	@Autowired
+	IUserRepository userRepo;
 
-	public boolean validateUser(String userID, String password) {
-		if (userID.equals("prueba") && password.equals("prueba"))
-			return true;
-		return false;
+	public boolean validateUser(String idUser, String password) {
+		UserEntity ue = userRepo.getUserById(idUser);
+		if(ue == null)
+			return false;
+		if(!ue.getPassword().equals(password))
+			return false;
+		return true;
 	}
 }
