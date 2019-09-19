a879f4399532178671b2d3465b1ac07521ba4533
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-18 21:10:39 -0500

    Test path

--- Veterinaria/src/main/java/Comp3/ServiceWeb/Veterinaria/Controller/GeneralController.java
+++ Veterinaria/src/main/java/Comp3/ServiceWeb/Veterinaria/Controller/GeneralController.java
@@ -3,6 +3,8 @@ package Comp3.ServiceWeb.Veterinaria.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class GeneralController {
@@ -11,5 +13,12 @@ public class GeneralController {
 	public String showIndex() {
 		return "index";
 	}
+	
+	@RequestMapping("/hola")
+	@ResponseBody
+	public String nada(@SessionAttribute("idUser") String idUser) {
+		return idUser;
+	}
+	
 	
 }
