63d567bb97aaaaece29bd7bb4b4bb51ce7a9f91c
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-30 22:18:39 -0500

    Test CryptoService

--- API/src/test/java/TT2018B003/comp3/API/TestCrypto/CryptoServiceTest.java
+++ API/src/test/java/TT2018B003/comp3/API/TestCrypto/CryptoServiceTest.java
@@ -1,6 +1,7 @@
 package TT2018B003.comp3.API.TestCrypto;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +18,18 @@ public class CryptoServiceTest {
 	CryptoService cryptoService;
 	
 	@Test
+	@Ignore
 	public void decryptTest() {
-		String encryptedMessage = "woafjaX37sb977td12+KpMyUTSujICKPAM+VCG2ouqz409/YQYPtcWS8SLpky4RCli1yctvbjrGVUWc3JQAGf3oqNHLMr4SOtlLF2iatPrFLTdZyYtbSiqLvC0dZx3bxhgIohp/MgxaRawszs9+RG+kmPXg/Y/zCqG+eQQqVyicECn9rHeJ6CXUFvEDM783S4mtiieyHHw6NKkyznDkXk6LPPLXDJYGGraZjwU8z0zWaPAxqH9toKkjubbZeG2iMRRr1LVh9W03abXFrgOpVcBtd1oakk8hKEtBWIpishni2hsQ3J4ju1Eu9sBDjjb6FxnwdPONqkz1agA3K9IN+EQ==";
+		String encryptedMessage = "vjPb9kR2lz0/EF0Fp09fQoG+lfJsWXOAnQ+31YE2niSB+e1xijfTFxb2gCUNGHI3vqRe3hd2lK0qyEz9sjlG6dSSEZBHhvhb+ZZmqKRGhDzKa1wf/QF/yTifX0v+AQ4Ba9Fl64c32pmN1xfkSTwe4+vtrD7FAHf7E/PALT2jJn3ipPskCVAn49PbDtSRZbBsqy5b1lWo1yMDLyDVBqqavhmCezfOarEyslLzan4SEZU6DBmHoxEIarL6ybxdqAtdlATB1SzLR5DlE0JTRHLl7lkUlWGa7rDRKBVR2E6iN+wi6zA9b4hfNgM8i+x+xCsN0r/gSPctNbW296WItLh2gg==";
 		String decryptedMessage = cryptoService.decryptAESKey(encryptedMessage);
 		
-		Assert.assertEquals("chiale", decryptedMessage);
+		Assert.assertEquals("zapatito12345678", decryptedMessage);
 	}
 	
 	@Test
 	public void decryptPattern() {
-		String aesKey = "chialechialechia";
-		String encryptedPattern = "U2FsdGVkX19FYn7+cApP7HTacvZoUEbfjYsIv1NqEdM=";
+		String aesKey = "chialevaledor";
+		String encryptedPattern = "U2FsdGVkX18zPrcPTwQRm5nFri/7CVwGKz9F2Kvj4GM=";
 		
 		String pattern = cryptoService.decryptPattern(encryptedPattern, aesKey);
 		
